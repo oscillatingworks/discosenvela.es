a945a5f09120b939d22bb6b8666c34e4d4281724
fern4lvarez <fernando@hey.com>
2023-05-15 20:50:54 +0200

    Renew website

---
+++ js/vela.js
@@ -0,0 +1,9 @@
+$(document).mousemove(function(event) {
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+    
+    mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+    mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+    
+    $('.radial-gradient').css('background', 'radial-gradient(circle at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #e5f96a,5%,#7e8aed,95%,#ECECE2)');
+});
