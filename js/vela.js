b96cf6655ac1440b4d3336a717ad9d4649ebbad3
fern4lvarez <fernando@hey.com>
2023-05-16 17:41:09 +0200

    Keep mouse location after clicks

--- js/vela.js
+++ js/vela.js
@@ -4,6 +4,15 @@ $(document).mousemove(function(event) {
     
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-    
+    sessionStorage.setItem("mouseX", mouseXpercentage);
+    sessionStorage.setItem("mouseY", mouseYpercentage);
+
+    $('.radial-gradient').css('background', `radial-gradient(circle at ${mouseXpercentage}% ${mouseYpercentage}%, #e5f96a,4%,#7e8aed,95%,#ECECE2)`);
+});
+
+$(document).ready(function(event) {
+    mouseXpercentage = sessionStorage.getItem("mouseX");
+    mouseYpercentage = sessionStorage.getItem("mouseY");
+
     $('.radial-gradient').css('background', `radial-gradient(circle at ${mouseXpercentage}% ${mouseYpercentage}%, #e5f96a,4%,#7e8aed,95%,#ECECE2)`);
 });
