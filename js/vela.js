11a42ab85cae772478600c6dc13c8da27669c6f4
fern4lvarez <fernando@hey.com>
2023-05-16 09:54:20 +0200

    Add improvements on header

--- js/vela.js
+++ js/vela.js
@@ -5,5 +5,5 @@ $(document).mousemove(function(event) {
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
     
-    $('.radial-gradient').css('background', 'radial-gradient(circle at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #e5f96a,4%,#7e8aed,95%,#ECECE2)');
+    $('.radial-gradient').css('background', `radial-gradient(circle at ${mouseXpercentage}% ${mouseYpercentage}%, #e5f96a,4%,#7e8aed,95%,#ECECE2)`);
 });
