bb18e7406300e37b0ad4078e63e6cfb6c704fa38
fern4lvarez <fernando@hey.com>
2023-05-19 00:50:11 +0200

    Cleanup and improve

--- js/vela.js
+++ js/vela.js
@@ -4,10 +4,19 @@ $(document).mousemove(function(event) {
     
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-    sessionStorage.setItem("mouseX", mouseXpercentage);
-    sessionStorage.setItem("mouseY", mouseYpercentage);
 
     $('.radial-gradient').css('background', `radial-gradient(circle at ${mouseXpercentage}% ${mouseYpercentage}%, #e5f96a,4%,#7e8aed,95%,#ECECE2)`);
+});
+
+$(document).click(function(event) {
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+
+    mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+    mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+
+    sessionStorage.setItem("mouseX", mouseXpercentage);
+    sessionStorage.setItem("mouseY", mouseYpercentage);
 });
 
 $(document).ready(function(event) {
