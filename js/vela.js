e6a03a4887f24b84a15752c6fbb6497df675be1e
fern4lvarez <fernando@hey.com>
2023-05-19 03:38:39 +0200

    Add information

--- js/vela.js
+++ js/vela.js
@@ -5,7 +5,7 @@ $(document).mousemove(function(event) {
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
 
-    $('.radial-gradient').css('background', `radial-gradient(circle at ${mouseXpercentage}% ${mouseYpercentage}%, #e5f96a,4%,#7e8aed,95%,#ECECE2)`);
+    $('.body-gradient').css('background', `radial-gradient(circle at ${mouseXpercentage}% ${mouseYpercentage}%, #e5f96a,4%,#7e8aed,95%,#ECECE2)`);
 });
 
 $(document).click(function(event) {
@@ -23,5 +23,9 @@ $(document).ready(function(event) {
     mouseXpercentage = sessionStorage.getItem("mouseX");
     mouseYpercentage = sessionStorage.getItem("mouseY");
 
-    $('.radial-gradient').css('background', `radial-gradient(circle at ${mouseXpercentage}% ${mouseYpercentage}%, #e5f96a,4%,#7e8aed,95%,#ECECE2)`);
+    $('.body-gradient').css('background', `radial-gradient(circle at ${mouseXpercentage}% ${mouseYpercentage}%, #e5f96a,4%,#7e8aed,95%,#ECECE2)`);
+});
+
+$('.no-body-gradient').ready(function(event) {
+    $('a').css({'cursor': 'pointer'})
 });
